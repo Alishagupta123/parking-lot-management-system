fda2498a7ebc02c77f2001725cbdd8d2ab08f5c3
Alishagupta123 <128981768+Alishagupta123@users.noreply.github.com>
2024-02-02 20:56:55 +0530

    Update parking_update_info.sql

--- project/database/parking_update_info.sql
+++ project/database/parking_update_info.sql
@@ -1,23 +1,8 @@
--- phpMyAdmin SQL Dump
--- version 5.1.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Mar 06, 2022 at 05:42 PM
--- Server version: 10.4.22-MariaDB
--- PHP Version: 8.1.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
+-
 -- Database: `parking_update_info`
 --
 
@@ -44,7 +29,3 @@ INSERT INTO `update_info` (`Owner_name`, `Vehicle_name`, `Vehicle_number`, `Entr
 ('akash kumar', 'Creta', 'UP 83 AT 4083', '2022-02-04 13:11:00', '2022-02-04 13:16:00', 1),
 ('avi', 'suzuki', 'UP 83 AT 4083', '2022-02-03 23:56:00', '2022-02-04 19:56:00', 34);
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
